559fe505b78698dd3c4ed9f6210d9c606aee4fab
Sigmond Ruphis <ops@foxframe.net>
2025-12-30 23:14:54 -0800

    Update sessionsData.js to include Session 3

--- src/data/sessionsData.js
+++ src/data/sessionsData.js
@@ -7,7 +7,7 @@
 const sessionsData = [
   { id: 1, title: 'Tithes for the Heir', path: '/sessions/session-1.md' },
   { id: 2, title: 'A Riven Transformation', path: '/sessions/session-2.md' },
+  { id: 3, title: 'The Warehouse & The Ledger', path: '/sessions/session-3.md' },
 ];
 
 export default sessionsData;
-
